ae6419ed6b5c046d327d5f85750f508e20c7d9d5
Dejan Milicic <dejan.milicic@gmail.com>
2016-04-01 00:49:09 +0200

    Customer model

---
+++ DapperDriven/DapperDriven/Model/Customer.cs
@@ -0,0 +1,21 @@
+﻿
+namespace DapperDriven.Model
+{
+	public class Customer
+	{
+        // key
+        public int CustomerId { get; set; } 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Company { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public int SupportRepId { get; set; }
+	}
+}
